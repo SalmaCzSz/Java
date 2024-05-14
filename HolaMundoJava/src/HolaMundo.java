b977d9338ceadd7590cbe496d5b32696e17b1615
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-14 08:27:37 -0600

    14/05/2024 | Inferencia de tipos de dato

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -5,5 +5,26 @@
 public class HolaMundo {
     public static void main(String args[]){
         System.out.println("Hola mundo c:");
+        
+        // definición de variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        
+        // modificando el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+        
+        // inferencia de tipos
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena2 = "Adios";
+        System.out.println(miVariableCadena2);
     }
 }
