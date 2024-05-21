47a4863ae425ec53fe7b05c5be8be5c15e5dc337
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-21 08:15:57 -0600

    21/05/2024 | Tipos primitivos > enteros

---
+++ HolaMundoJava/src/TiposPrimitivos.java
@@ -0,0 +1,29 @@
+public class TiposPrimitivos {
+    public static void main(String args[]){
+        /*
+        * Tipos primitivos
+        * */
+        byte numByte = (byte)10;
+        System.out.println("Valor byte:" + numByte);
+        System.out.println("Valor minimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte: " + Byte.MAX_VALUE);
+
+
+        short numShort = (short) 32768;
+        System.out.println("Valor short: " + numShort);
+        System.out.println("Valor minimo short: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo short: " + Short.MAX_VALUE);
+
+
+        int numInt = (int)2147483648L; // L -> literal de tipo long
+        System.out.println("Valor int: " + numInt);
+        System.out.println("Valor minimo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo int: " + Integer.MAX_VALUE);
+
+
+        long numLong = 10;
+        System.out.println("Valor long: " + numLong);
+        System.out.println("Valor minimo long: " + Long.MIN_VALUE);
+        System.out.println("Valor maximo long: " + Long.MAX_VALUE);
+    }
+}
