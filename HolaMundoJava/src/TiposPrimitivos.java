be5a0e0280d58ed8bf6a6b38d094aabd3246b46b
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-21 08:26:32 -0600

    21/05/2024 | Tipos primitivos > flotantes

--- HolaMundoJava/src/TiposPrimitivos.java
+++ HolaMundoJava/src/TiposPrimitivos.java
@@ -1,7 +1,7 @@
 public class TiposPrimitivos {
     public static void main(String args[]){
         /*
-        * Tipos primitivos
+        * Tipos primitivos enteros
         * */
         byte numByte = (byte)10;
         System.out.println("Valor byte:" + numByte);
@@ -25,5 +25,21 @@ public class TiposPrimitivos {
         System.out.println("Valor long: " + numLong);
         System.out.println("Valor minimo long: " + Long.MIN_VALUE);
         System.out.println("Valor maximo long: " + Long.MAX_VALUE);
+
+
+
+        /*
+         * Tipos primitivos flotantes
+         * */
+        float numFloat = 10.0F; // F -> literal de tipo float
+        System.out.println("Valor float: " + numFloat);
+        System.out.println("Valor minimo float: " + Float.MIN_VALUE);
+        System.out.println("Valor maximo float: " + Float.MAX_VALUE);
+
+
+        double numDouble = 1.7676931348; // tipo de dato mas grande
+        System.out.println("Valor double: " + numDouble);
+        System.out.println("Valor minimo double: " + Double.MIN_VALUE);
+        System.out.println("Valor maximo double: " + Double.MAX_VALUE);
     }
 }
