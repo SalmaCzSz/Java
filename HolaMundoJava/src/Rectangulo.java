5773d45f755b48f383cd4c6c474f6aea0fe59884
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-05-29 07:39:36 -0600

    29/05/2024 | Tarea rectangulo

---
+++ HolaMundoJava/src/Rectangulo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Rectangulo {
+    public static void main(String args[]){
+        Scanner lee = new Scanner(System.in);
+        int alto;
+        int ancho;
+        int area;
+        int perimetro;
+
+        System.out.print("Proporciona el alto: ");
+        alto = lee.nextInt();
+        System.out.print("Proporciona el ancho: ");
+        ancho = lee.nextInt();
+
+        area = ancho * alto;
+        perimetro = (ancho + alto) * 2;
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimetro = " + perimetro);
+    }
+}
