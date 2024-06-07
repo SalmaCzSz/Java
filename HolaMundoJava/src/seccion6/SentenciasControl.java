bc7bc7efe658ac584c4a05c2b3052eb5bf99cbc9
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-07 11:24:48 -0600

    07/06/2024 | Seccion 6: sentencias de control

---
+++ HolaMundoJava/src/seccion6/SentenciasControl.java
@@ -0,0 +1,14 @@
+package seccion6;
+
+public class SentenciasControl {
+    public static void main(String args[]){
+        var condicion = true;
+
+        if(condicion){
+            System.out.println("Condición verdadera");
+        }
+        else {
+            System.out.println("Condición falsa");
+        }
+    }
+}
