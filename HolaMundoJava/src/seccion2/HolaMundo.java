ef2893f2207dd13dc06cf91aa9b26a625cdb31e6
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-07 11:09:04 -0600

    07/06/2024 | Seccion 2: hola mundo

---
+++ HolaMundoJava/src/seccion2/HolaMundo.java
@@ -0,0 +1,13 @@
+package seccion2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salma
+ */
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola mundo c:");
+    }
+}
