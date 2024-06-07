7182ff9b77b2ab2d66dd2ba1e41503c461cf99d5
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-07 11:12:02 -0600

    07/06/2024 | Seccion 3: variables

---
+++ HolaMundoJava/src/seccion3/Variables.java
@@ -0,0 +1,69 @@
+package seccion3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salma
+ */
+public class Variables {
+    public static void main(String args[]){
+        // definicion de variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        
+        // modificando el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+        
+        
+        // inferencia de tipos
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena2 = "Adios";
+        System.out.println(miVariableCadena2);
+
+        
+        // concatenacion
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+
+        var i = 3;
+        var j = 4;
+        System.out.println(i + j); // 7 -> devuelve la suma
+        System.out.println(i + j + usuario); // 7Juan -> suma y concatena el nombre
+        System.out.println(usuario + i + j); // Juan34 -> concatena toda la cadena
+        System.out.println(usuario + (i + j)); // Juan7 -> concatena la cadena y el resultado de la suma
+
+
+        // caracteres especiales
+        String nombre = "Karla";
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \'" + nombre + "\'");
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
+
+        
+        // Scanner
+        Scanner lee = new Scanner(System.in);
+        var user = "";
+        var title = "";
+
+        System.out.print("Escribe tu nombre: ");
+        user = lee.nextLine();
+        System.out.println("usuario = " + user);
+        System.out.print("Escribe tu titulo: ");
+        title = lee.nextLine();
+        System.out.println("titulo = " + title);
+    }
+}
