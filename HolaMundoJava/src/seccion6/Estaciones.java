b0a7311260d594eab1019a2a1fa35b0dbfd65ce5
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-10 08:38:40 -0600

    10/06/2024 | Seccion 6: if - else

---
+++ HolaMundoJava/src/seccion6/Estaciones.java
@@ -0,0 +1,20 @@
+package seccion6;
+
+public class Estaciones {
+    public static void main(String args[]) {
+        var mes = 1;
+        var estacion = "Estación desconocida";
+        
+        if(mes == 1 || mes == 2 || mes == 12){
+            estacion = "Invierno";
+        } else if(mes == 3 || mes == 4 || mes == 5){
+            estacion = "Primavera";
+        } else if(mes == 6 || mes == 7 || mes == 8){
+            estacion = "Verano";
+        } else if(mes == 9 || mes == 10 || mes == 11){
+            estacion = "Otonio";
+        }
+
+        System.out.println("estacion = " + estacion);
+    }
+}
