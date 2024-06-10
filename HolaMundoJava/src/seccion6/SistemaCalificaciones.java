5564d8e32befb9d59b3cb310edaa3902eb4a24e2
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-10 09:52:08 -0600

    10/06/2024 | Seccion 6: sistema de calificiaciones

--- HolaMundoJava/src/seccion6/SistemaCalificaciones.java
+++ HolaMundoJava/src/seccion6/SistemaCalificaciones.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class SistemaCalificaciones {
     public static void main(String args[]) {
         Scanner lee = new Scanner(System.in);
-        var calificacion = 0;
+        var calificacion = 0.0;
         var nota = "Valor desconocido";
 
         System.out.print("Ingresa un valor entre 0 y 10: ");
-        calificacion = Integer.parseInt(lee.nextLine());
+        calificacion = Double.parseDouble(lee.nextLine());
 
-        switch (calificacion){
+        /*switch (calificacion){
             case 9: case 10:
                 nota = "A";
                 break;
@@ -27,6 +27,18 @@ public class SistemaCalificaciones {
             case 0: case 1: case 2: case 3: case 4: case 5:
                 nota = "F";
                 break;
+        } */
+
+        if (calificacion >= 0 && calificacion < 6){
+            nota = "F";
+        } else if (calificacion >= 6 && calificacion < 7){
+            nota = "D";
+        } else if (calificacion >= 7 && calificacion < 8){
+            nota = "C";
+        } else if (calificacion >= 8 && calificacion < 9){
+            nota = "B";
+        } else if (calificacion >= 9 && calificacion <= 10){
+            nota = "A";
         }
 
         System.out.println("nota = " + nota);
