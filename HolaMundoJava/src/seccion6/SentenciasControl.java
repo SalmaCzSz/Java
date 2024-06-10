0c30514c044b966c4816124922b87d878b2a7ea2
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-10 08:28:00 -0600

    10/06/2024 | Seccion 6: if - else

--- HolaMundoJava/src/seccion6/SentenciasControl.java
+++ HolaMundoJava/src/seccion6/SentenciasControl.java
@@ -10,5 +10,20 @@ public class SentenciasControl {
         else {
             System.out.println("Condición falsa");
         }
+        
+        var numero = 2;
+        var numeroTexto = "Numero desconocido";
+        
+        if(numero == 1){
+            numeroTexto = "Numero uno";
+        } else if(numero == 2){
+            numeroTexto = "Numero dos";
+        } else if(numero == 3){
+            numeroTexto = "Numero tres";
+        }  else if(numero == 4){
+            numeroTexto = "Numero cuatro";
+        }
+
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
