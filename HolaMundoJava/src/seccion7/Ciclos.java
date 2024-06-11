cc0f7eabcaec7f7f69ee52fab00f49e459b7a186
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-11 07:22:35 -0600

    11/06/2024 | Ciclos > while

---
+++ HolaMundoJava/src/seccion7/Ciclos.java
@@ -0,0 +1,15 @@
+package seccion7;
+
+public class Ciclos {
+    public static void main(String args[]) {
+        /*
+        * While
+        * */
+        var contador = 0;
+
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+    }
+}
