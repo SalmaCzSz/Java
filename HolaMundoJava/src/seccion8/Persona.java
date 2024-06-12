559e2b14cbe6e6b999b1e3fdda47c721346888b9
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-12 08:11:51 -0600

    12/06/2024 | seccion 8: clases

---
+++ HolaMundoJava/src/seccion8/Persona.java
@@ -0,0 +1,17 @@
+package seccion8;
+
+public class Persona {
+    /*
+    * Atributos
+    * */
+    String nombre;
+    String apellido;
+
+    /*
+    * Metodos
+    * */
+    public void desplegarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
---
+++ HolaMundoJava/src/seccion8/pruebaPersona.java
@@ -0,0 +1,12 @@
+package seccion8;
+
+import seccion8.Persona;
+
+public class pruebaPersona {
+    public static void main(String[] args){
+        Persona persona1 = new Persona();
+        persona1.nombre = "Salma";
+        persona1.apellido = "Cruz";
+        persona1.desplegarInformacion();
+    }
+}
