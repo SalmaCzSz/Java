534aebb0958dbf52c1f4a67801931a4abd034c7c
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-12 08:22:40 -0600

    12/06/2024 | seccion 8: clases

--- HolaMundoJava/src/seccion8/pruebaPersona.java
+++ HolaMundoJava/src/seccion8/pruebaPersona.java
@@ -8,5 +8,15 @@ public class pruebaPersona {
         persona1.nombre = "Salma";
         persona1.apellido = "Cruz";
         persona1.desplegarInformacion();
+        
+        Persona persona2 = new Persona();
+
+        System.out.println("persona1 = " + persona1); // @referencia en memoria del objeto
+        System.out.println("persona2 = " + persona2); // @referencia en memoria del objeto
+
+        persona2.desplegarInformacion();
+        persona2.nombre = "Thamar";
+        persona2.apellido = "Sanchez";
+        persona2.desplegarInformacion();
     }
 }
