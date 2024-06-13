1ba7084835023fa0540a8c535fc6d39e15237737
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-06-13 07:37:26 -0600

    13/06/2024 | seccion 9: metodos

---
+++ Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,17 @@
+package Operaciones;
+
+public class Aritmetica {
+    /*
+    * Atributos
+    */
+    int a;
+    int b;
+    
+    /*
+    * Metodos
+    */
+    public void sumar(){// void no regresa informacion
+       int resultado = a + b;
+       System.out.println("resultado = " + resultado);
+    }
+}
