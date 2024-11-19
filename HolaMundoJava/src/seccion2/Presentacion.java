8eaf56bac6bb302a304008d6b8e64ae572196b6a
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-11-19 10:30:33 -0600

    19/11/2024 | Ejercicio presentacion

---
+++ HolaMundoJava/src/seccion2/Presentacion.java
@@ -0,0 +1,13 @@
+package seccion2;
+
+/**
+ *
+ * @author Salma
+ */
+public class Presentacion {
+    public static void main(String args[]){
+        System.out.println("Salma Cruz Sanchez");
+        System.out.println("25 anios");
+        System.out.println("MX");
+    }
+}
