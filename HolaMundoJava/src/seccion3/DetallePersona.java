f77b8cd1f6a13b2de2352b9268950bc3e68e390c
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-11-21 09:19:37 -0600

    20/11/2024 | Reto detallePersona

---
+++ HolaMundoJava/src/seccion3/DetallePersona.java
@@ -0,0 +1,17 @@
+package seccion3;
+
+public class DetallePersona {
+    public static void main(String[] args) {
+        String nombre = "Salma Cruz";
+        int edad = 25;
+        float altura = 1.59f;
+        String paisOrigen = "MX";
+        char estado = 'S';
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(estado);
+    }
+}
