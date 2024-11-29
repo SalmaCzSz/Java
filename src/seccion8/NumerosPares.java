62094b7ca37df9854777b0b1ca3fe21a12420afe
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-11-29 11:57:41 -0600

    29/11/2024 | Impresion de numeros pares con ciclo for

--- src/seccion8/NumerosPares.java
+++ src/seccion8/NumerosPares.java
@@ -11,5 +11,14 @@ public class NumerosPares {
             }
             contador++;
         }
+
+        System.out.println("\n\n* * * Numeros pares del 0 al 20");
+
+        for(int i = 0; i <= 20; i ++){
+            if(i % 2 == 0){
+                System.out.print(i + " ");
+            }
+            i++;
+        }
     }
 }
